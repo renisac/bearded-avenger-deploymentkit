9e016c21c221b5921fbfff4673c3ad0656eb9e20
Chris O'Donnell <chodonne@gmail.com>
2020-11-03 13:06:28 -0500

    fix: elasticsearch and sqlite backends both work

--- s6/etc-cont-init.d/01-sqlite-token-update.sh
+++ s6/etc-cont-init.d/01-sqlite-token-update.sh
@@ -11,9 +11,12 @@ fi
 CIF_TOKEN="${CIF_TOKEN}"
 CIF_HUNTER_TOKEN="${CIF_HUNTER_TOKEN}"
 CSIRTG_SMRT_TOKEN="${CSIRTG_SMRT_TOKEN}"
+CIF_HTTPD_TOKEN="${CIF_HTTPD_TOKEN}"
 
 if [ -n "${CIF_TOKEN}" ]
 then
+  echo ""
+  echo "set admin token"
   echo "---" > /home/cif/.cif.yml
   echo "token: ${CIF_TOKEN}" >> /home/cif/.cif.yml
   sqlite3 /var/lib/cif/cif.db "UPDATE tokens SET token = \"${CIF_TOKEN}\" WHERE username = 'admin';" ".exit"
@@ -21,6 +24,8 @@ fi
 
 if [ -n "${CIF_HUNTER_TOKEN}" ]
 then
+  echo ""
+  echo "set hunter token"
   echo "---" > /etc/cif/cif-router.yml
   echo "hunter_token: ${CIF_HUNTER_TOKEN}" >> /etc/cif/cif-router.yml
   sqlite3 /var/lib/cif/cif.db "UPDATE tokens SET token = \"${CIF_HUNTER_TOKEN}\" WHERE username = 'hunter';" ".exit"
@@ -28,6 +33,8 @@ fi
 
 if [ -n "${CSIRTG_SMRT_TOKEN}" ]
 then
+  echo ""
+  echo "set smrt token"
   echo "---" > /etc/cif/csirtg-smrt.yml
   echo "token: ${CSIRTG_SMRT_TOKEN}" >> /etc/cif/csirtg-smrt.yml
   sqlite3 /var/lib/cif/cif.db "UPDATE tokens SET token = \"${CSIRTG_SMRT_TOKEN}\" WHERE username = 'csirtg-smrt';" ".exit"
@@ -35,9 +42,9 @@ fi
 
 if [ -n "${CIF_HTTPD_TOKEN}" ]
 then
-#  echo "---" > /etc/cif/csirtg-smrt.yml
-#  echo "token: ${CSIRTG_SMRT_TOKEN}" >> /etc/cif/csirtg-smrt.yml
-#  sqlite3 /var/lib/cif/cif.db "UPDATE tokens SET token = \"${CIF_HTTPD_TOKEN}\" WHERE EXISTS username = 'httpd';" ".exit"
-  sqlite3 /var/lib/cif/cif.db "INSERT INTO token (username,token) VALUES('httpd',\"${CIF_HTTPD_TOKEN}\");" ".exit"
+  echo ""
+  echo "set httpd token"
+  sqlite3 /var/lib/cif/cif.db "INSERT INTO tokens (username,token) VALUES('httpd',\"${CIF_HTTPD_TOKEN}\");" ".exit"
 fi
 
+echo ""
