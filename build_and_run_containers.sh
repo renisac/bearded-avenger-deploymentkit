7dc90d6c1ef17800859444940e945c7f54409787
Chris O'Donnell <chodonne@gmail.com>
2023-04-11 16:25:57 -0400

    fix: modify Docker provisioning script

--- build_and_run_containers.sh
+++ build_and_run_containers.sh
@@ -15,6 +15,6 @@ docker-compose -f docker-compose.yml build cif-smrt
 docker-compose -f docker-compose.yml pull proxy-es
 docker-compose -f docker-compose.yml pull es01
 
-docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
+docker-compose -f docker-compose.yml up -d
 
 docker-compose -f docker-compose.yml logs -f
